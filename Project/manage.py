963d7d8efd58c3ae2bcddb430fddac32c0209652
HarryLam14 <hlam0478@gmail.com>
2021-04-01 16:53:21 +0100

    Restarting without mongo

--- Project/manage.py
+++ Project/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Djongo_Project.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

45f9c488ada71937b6498eeba8d0f1686285865d
AerospacedOut <michael.j.winebrenner@gmail.com>
2021-04-01 11:08:16 +0100

    data cleaner now processes review information and joins tables

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,9 +5,16 @@ interaction = pd.read_csv("RAW_interactions.csv")
 recipes = pd.read_csv("RAW_recipes.csv")
 
 # Removes unwated columns
-interaction.drop(["review","user_id","date"],axis=1,inplace=True)
-recipes.drop(["contributor_id","submitted","tags","n_steps","n_ingredients"],axis=1,inplace=True)
+interaction.drop(["review","user_id","date"], axis=1, inplace=True)
+recipes.drop(["contributor_id","submitted","tags","n_steps","n_ingredients"], axis=1, inplace=True)
+
+ratings = interaction.groupby("recipe_id").sum() / interaction.groupby("recipe_id").count()
+ratings["n_reviews"] = interaction.groupby("recipe_id").count()
+print(ratings.head())
+
+preprocessed_data = pd.merge(left=recipes, right=ratings, how='left', left_on='id', right_on='recipe_id')
+preprocessed_data.drop(["id"], axis=1, inplace=True)
+print(preprocessed_data.head())
 
 # Creates new csv and saves cleaned data to it
-interaction.to_csv("CLEANED_interactions.csv",index=False)
-recipes.to_csv("CLEANED_recipes.csv",index=False)
+preprocessed_data.to_csv("CLEANED_JOINED_data.csv", index=False)

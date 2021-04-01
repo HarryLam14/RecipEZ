00006f52f4e70f17a82bc402bb3a7a2a021994f7
Matt <mjpbaines@sky.com>
2021-04-01 10:26:53 +0100

    updated data cleaner

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+# Loads the raw data into a pandas dataframe
 interaction = pd.read_csv("RAW_interactions.csv")
+recipes = pd.read_csv("RAW_recipes.csv")
 
+# Removes unwated columns
 interaction.drop(["review","user_id","date"],axis=1,inplace=True)
+recipes.drop(["contributor_id","submitted","tags","n_steps","n_ingredients"],axis=1,inplace=True)
 
+# Creates new csv and saves cleaned data to it
 interaction.to_csv("CLEANED_interactions.csv",index=False)
+recipes.to_csv("CLEANED_recipes.csv",index=False)

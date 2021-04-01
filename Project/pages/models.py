49626c9de49adff8c6a268c0fa62ed764172ee0f
HarryLam14 <hlam0478@gmail.com>
2021-04-01 17:32:46 +0100

    adding models

--- Project/pages/models.py
+++ Project/pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Recipes(models.Model):
+    recipe_id = models.IntegerField(primary_key=True)
+    recipe_name = models.CharField(max_length = 200)
+    steps = models.CharField(max_length = 200)
+
+class Ingredients(models.Model):
+    ingredient_id = models.IntegerField(primary_key=True)
+    ingredient_name = models.CharField(max_length = 200)
+    recipes = models.ManyToManyField(Recipes)
+
+
+# class Join(models.Model):
+#     recipe_id = models.ForeignKey()
+#     ingredient_id = models.ForeignKey()

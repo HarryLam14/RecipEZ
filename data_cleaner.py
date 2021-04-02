533a98b7ef40267ca0b70e4c64cb0718b783cae7
Adam <adamgirach@gmail.com>
2021-04-02 17:23:44 +0100

    Updated data_cleaner.py to actually get rid of quotes surrounding lists this time, got rid of unwanted whitespaces and \n characters, changed output to JSON because CSV was adding quotes around lists changing them to strings

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import ast
 
 # Loads the raw data into a pandas dataframe
 interaction = pd.read_csv("RAW_interactions.csv")
@@ -8,8 +9,13 @@ recipes = pd.read_csv("RAW_recipes.csv")
 interaction.drop(["review","user_id","date"], axis=1, inplace=True)
 recipes.drop(["contributor_id","submitted","tags","n_steps","n_ingredients"], axis=1, inplace=True)
 
-# Strip quotes from ingredient lists
-recipes['ingredients'] = recipes['ingredients'].map(lambda x: x.strip('"'))
+# Strip quotes from ingredients, nutrition and steps lists; finally lists are recognised as list types and not string types;
+# Strips unwanted whitespaces and \n characters from name and description
+recipes['ingredients'] = recipes['ingredients'].map(lambda x: ast.literal_eval(x))
+recipes['nutrition'] = recipes['nutrition'].map(lambda x: ast.literal_eval(x))
+recipes['steps'] = recipes['steps'].map(lambda x: ast.literal_eval(x))
+recipes['name'] = recipes['name'].map(lambda x: ' '.join(str(x).split()))
+recipes['description'] = recipes['description'].map(lambda x: ' '.join(str(x).split()))
 
 
 # Find average rating of each recipe
@@ -21,5 +27,7 @@ ratings["n_reviews"] = interaction.groupby("recipe_id").count()
 preprocessed_data = pd.merge(left=recipes, right=ratings, how='left', left_on='id', right_on='recipe_id')
 preprocessed_data.drop(["id"], axis=1, inplace=True)
 
-# Creates new csv and saves cleaned data to it
-preprocessed_data.to_csv("CLEANED_JOINED_data.csv", index=False)
+# Creates new json and saves cleaned data to it;
+# indent prettifies the data, orient changes the notation of the data
+# (values for orient='split', 'records', 'index', 'columns', 'values', 'table')
+preprocessed_data.to_json("CLEANED_JOINED_data.json", orient='index', indent=4)
